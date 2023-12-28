8daa5af4cbd12a4c8e72b633255eff7cf19729aa
ShinjungOh <tlswjd9260@gmail.com>
2023-12-29 06:02:22 +0900

    feat: 검색 페이지 UI 구현

--- src/app/(afterLogin)/[username]/page.tsx
+++ src/app/(afterLogin)/[username]/page.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import style from './profile.module.css';
 import Post from "@/app/(afterLogin)/_component/Post";
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
 
 export default function Profile() {
     const user = {
@@ -10,20 +9,10 @@ export default function Profile() {
         image: '/myProfile.jpg'
     }
 
-    const onClick = () => {}
-
     return (
         <main className={style.main}>
             <div className={style.header}>
-                {/*<BackButton/>*/}
-                <button className={style.backButton} onClick={onClick}>
-                    <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
-                         className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
-                        <g>
-                            <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"/>
-                        </g>
-                    </svg>
-                </button>
+                <BackButton />
                 <h3 className={style.headerTitle}>{user.nickname}</h3>
             </div>
             <div className={style.userZone}>
