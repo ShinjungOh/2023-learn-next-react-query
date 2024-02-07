e97dc80c13bb23dbe8ce443b214fd829c835ad7d
ShinjungOh <tlswjd9260@gmail.com>
2024-02-07 23:51:26 +0900

    feat: next-auth로 로그인하기

--- src/app/(beforeLogin)/_component/LoginModal.tsx
+++ src/app/(beforeLogin)/_component/LoginModal.tsx
@@ -1,20 +1,43 @@
 "use client";
 
 import style from '@/app/(beforeLogin)/_component/login.module.css';
-import {useState} from "react";
+import {ChangeEventHandler, FormEventHandler, useState} from "react";
+import {useRouter} from "next/navigation";
+import {signIn} from "next-auth/react";
 
 export default function LoginModal() {
-    const [id, setId] = useState();
-    const [password, setPassword] = useState();
-    const [message, setMessage] = useState();
+    const [id, setId] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const router = useRouter();
 
-    const onSubmit = () => {};
+    const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        setMessage('');
+        try {
+            await signIn("credentials", {
+                username: id,
+                password,
+                redirect: false,
+            })
+            router.replace('/home');
+        } catch (e) {
+         console.error(e);
+         setMessage('아이디와 비밀번호가 일치하지 않습니다.')
+        }
+    };
 
-    const onClickClose = () => {};
+    const onClickClose = () => {
+        router.back();
+    };
 
-    const onChangeId = () => {};
+    const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setId(e.target.value);
+    };
 
-    const onChangePassword = () => {};
+    const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
+        setPassword(e.target.value);
+    };
 
     return (
         <div className={style.modalBackground}>
@@ -24,8 +47,7 @@ export default function LoginModal() {
                         <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
                              className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
                             <g>
-                                <path
-                                    d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
+                                <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"/>
                             </g>
                         </svg>
                     </button>
--- src/auth.ts
+++ src/auth.ts
@@ -6,5 +6,32 @@ export const {
     auth,
     signIn,
 } = NextAuth({
+    pages: {
+        signIn: '/i/flow/login',
+        newUser: '/i/flow/signup',
+    },
+    providers: [
+    CredentialsProvider({
+        async authorize(credentials) {
+            const authResponse = await fetch(`${process.env.AUTH_URL}/api/login`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    id: credentials.username,
+                    password: credentials.password,
+                }),
+            })
 
+            if (!authResponse.ok) {
+                return null // 로그인 실패 이유 추가
+            }
+
+            const user = await authResponse.json()
+
+            return user
+        },
+    }),
+    ]
 });
