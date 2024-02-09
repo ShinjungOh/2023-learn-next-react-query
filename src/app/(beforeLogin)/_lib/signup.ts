e2d969c585d2f0a73a5353b59d5681006e34ab3b
ShinjungOh <tlswjd9260@gmail.com>
2024-02-09 23:57:52 +0900

    feat: 로그인 유무에 따른 분기 처리, useSession 적용

--- src/app/(beforeLogin)/_lib/signup.ts
+++ src/app/(beforeLogin)/_lib/signup.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import {redirect} from "next/navigation";
+import {signIn} from "@auth";
 
 export default async (prevState: any, formData: FormData) => {
     if (!formData.get('id') || !(formData.get('id') as string)?.trim()) {
@@ -31,6 +32,11 @@ export default async (prevState: any, formData: FormData) => {
         console.log(response.status);
         console.log(await response.json());
         shouldRedirect = true;
+        await signIn("credentials", {
+            username: formData.get('id'),
+            password: formData.get('password'),
+            redirect: false,
+        })
     } catch (err) {
         console.log(err);
         return;
