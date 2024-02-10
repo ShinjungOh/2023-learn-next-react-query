b0a7b6ede7c2a8a81f83026a672708a29b2f1765
ShinjungOh <tlswjd9260@gmail.com>
2024-02-11 03:24:31 +0900

    feat: MSW, react-query 이용해 클라이언트에서 데이터 보여주기

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,29 +2,16 @@ import style from './home.module.css';
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
-import Post from "@/app/(afterLogin)/_component/Post";
 import {dehydrate, HydrationBoundary, QueryClient} from "@tanstack/react-query";
-
-async function getPostRecommends() {
-    const res = await fetch('http://localhost:9090/api/postRecommends', {
-        next: {
-            tags: ['post', 'recommends'],
-        }
-    })
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch data');
-    }
-
-    return res.json();
-}
+import {getPostRecommends} from "@/app/(afterLogin)/home/_lib/getPostRecommends";
+import PostRecommends from "@/app/(afterLogin)/home/_component/PostRecommends";
 
 export default async function Home() {
     const queryClient = new QueryClient();
-    await queryClient.prefetchQuery({queryKey: ['post', 'recommends'], queryFn: getPostRecommends});
+    await queryClient.prefetchQuery({queryKey: ['posts', 'recommends'], queryFn: getPostRecommends});
     const dehydratedState = dehydrate(queryClient);
 
-    queryClient.getQueryData(['post', 'recommends']);
+    queryClient.getQueryData(['posts', 'recommends']);
 
     return (
         <main className={style.main}>
@@ -32,11 +19,7 @@ export default async function Home() {
                 <TabProvider>
                     <Tab/>
                     <PostForm/>
-                    <Post/>
-                    <Post/>
-                    <Post/>
-                    <Post/>
-                    <Post/>
+                    <PostRecommends/>
                 </TabProvider>
             </HydrationBoundary>
         </main>
---
+++ src/model/PostImage.ts
@@ -0,0 +1,6 @@
+import { Post } from "./Post"
+export interface PostImage {
+    imageId: number;
+    link: string;
+    Post?: Post;
+}
